4520a88befbc3e5e7ca670816a3c1edac342ea18
yukuima <58396683@qq.com>
2019-06-05 21:54:29 +0800

    commit

---
+++ src/main/java/com/myk/learning/webflux/model/Message.java
@@ -0,0 +1,14 @@
+package com.myk.learning.webflux.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    String body;
+}
